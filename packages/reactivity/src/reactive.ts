ccf2a2ea0020562d3ef1d51c276534ffe3167a1a
ZhangFan <zhangfan2021@boco.com.cn>
2022-05-30 18:01:35 +0800

    实现watch

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -3,6 +3,11 @@ import { mutableHandlers,ReactiveFlags } from './baseHandler'
 //WeakMap()不会导致内存泄漏，弱引用，和map
 const reactiveMap=new WeakMap();
 
+//watch判断响应式数据的方法
+export function isReactive(value){
+    return !!(value && value[ReactiveFlags.IS_REACTIVE]) //取反
+}
+
 //讲数据转换成响应式数据，reactive只能做对象代理,和map很像，key只能是对象
 export function reactive(target){
     if(!isObject(target)){
---
+++ packages/reactivity/src/watch.ts
@@ -0,0 +1,44 @@
+//这个文件不用该出现在响应式的文件目录里
+
+import { isFunction, isObject } from "@vue/shared";
+import { ReactiveEffect } from "./effect";
+import { isReactive } from "./reactive";
+
+function traversal(value, set = new Set()) {
+  if (!isObject(value)) return value;
+  if (set.has(value)) {
+    //如果对象里有，那么就遍历过了，直接返回值
+    return value;
+  }
+  //否则，就添加
+  set.add(value);
+  for (let key in value) {
+    traversal(value[key]);
+  }
+
+  return value;
+}
+
+//source 使用户传入的对象，cb是对应用户的回调
+export function watch(source, cb) {
+  let getter;
+  //看一下传入的值是不是响应式的
+  if (isReactive(source)) {
+    //对用户传入的数据进行递归循环，就会访问对象上的每一个属性
+    //访问属性的时候就会手机effect
+    getter = () => traversal(source);
+  } else if (isFunction(source)) {
+    getter = source;
+  } else {
+    return;
+  }
+  let oldValue;
+  const job = () => {
+    const newValue = effect.run();
+    cb(newValue, oldValue);
+    oldValue = newValue;
+  };
+  //在effect中访问属性就可以依赖收集
+  const effect = new ReactiveEffect(getter, job);
+  oldValue = effect.run();
+}
